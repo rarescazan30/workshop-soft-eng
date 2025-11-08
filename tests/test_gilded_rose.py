bb820a2b4a347daa416657d1e3bfca7b4f5f2685
Rares Cazan <rarescazan30@gmail.com>
2025-11-08 11:46:42 +0200

    fix: refactor gilded_rose code for visibility; added tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -9,7 +9,27 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+
+    def test_brie(self):
+        items = [Item("brie", 30, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("brie", items[0].name)
+
+    def test_aged_brie_multiple_days(self):
+        items = [Item("Aged Brie", 100, 40)]
+        gilded_rose = GildedRose(items)
+        for days in range(15):
+            gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+    def test_sulphuras_multiple_days(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 100, 40)]
+        gilded_rose = GildedRose(items)
+        for days in range(15):
+            gilded_rose.update_quality()
+        self.assertEqual(80, items[0].quality)
+        self.assertEqual(100, items[0].sell_in)
 
         
 if __name__ == '__main__':
